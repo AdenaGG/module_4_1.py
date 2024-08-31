b0b586035e82264698f86c09e2fadb1fed20276a
Кахбек Халилов <profjeriko@outlook.com>
2024-08-31 23:49:16 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -3,10 +3,10 @@ from math import inf
 
 def divide(first, second):
     if second == 0:
-        return print('inf')
+        return inf
     else:
-        return print(first / second)
+        return first / second
 
 
-divide(int(input('Введите число td: ')), int(input("Введите число td 2: ")))
+divide(1, 1)
 
